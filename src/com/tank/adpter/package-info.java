bfa3343d90afa7c83ebcd0955ed2d29fb4409c97
forestnlp <19085498@qq.com>
2021-02-08 16:26:16 +0800

    bridge模式，实现2个不同层面的类爆炸的问题。用聚合取代继承

--- src/com/tank/adpter/package-info.java
+++ src/com/tank/adpter/package-info.java
@@ -3,6 +3,10 @@ package com.tank.adpter;
 /**
  * 就是一个转换器
  *
- * 比如asm中的adapter等
+ * 比如asm中的adapter等，适配了其他的vistor
+ *
+ *
+ * 误区：这不是适配器模式
+ * windowlistner 接口 --> window adapter 抽象类 （具体方法实现了listner的方法，只需要继承adapter类，再覆盖部分方法就可以了）
  *
  */
